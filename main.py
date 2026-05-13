2bb1abf143acce85dda29de39744b4c624aa5dde
choe66264 <choe66264@gmail.com>
2026-05-13 13:55:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 st.title('나의 첫 웹 서비스 만들기!')
 st.text_input('이름을 입력해주세요')
-st.selectbox('좋아하는 음식을 선택하세요!,['마라탕','치킨']
+st.selectbox('좋아하는 음식을 선택하세요!',['마라탕','치킨']
 if st.button('인사말 생성'):
   st.write(a+'님!, 안녕하세요, 반갑습니다!')
   st.info('반갑습니다')
